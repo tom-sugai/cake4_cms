045d7a8e1b27e04627b1403dfd066eb51596f537
Tom Sugai <tom.sugai@gmail.com>
2024-06-07 17:24:23 +0900

    comment out debug()

--- src/Controller/FileLoadController.php
+++ src/Controller/FileLoadController.php
@@ -14,12 +14,12 @@ class FileLoadController extends AppController
         if ($this->request->is('post')) {
             $file = $this->request->getData('csv'); //受け取り
             //$file = $this->request->getData(); 
-            debug($file);
+            //debug($file);
             $addToDir = $this->request->getData('addToDir');
-            debug($addToDir);
+            //debug($addToDir);
             //$filePath = '../webroot/img/' . date("YmdHis") . $file['name'];
             //$filePath = '../webroot/files/' . date("YmdH") . $file['name'];
-            debug($file['name']);
+            //debug($file['name']);
             $filePath = '../webroot/' . $addToDir . '/' . date("YmdH") . $file['name'];
             $rt = move_uploaded_file($file['tmp_name'], $filePath); //ファイル名の先頭に時間をくっつけて/webroot/$addToDirに移動させる
             if($rt){   
