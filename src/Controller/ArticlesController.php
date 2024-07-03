daac25b94e11b40731ffb0d2e4f4586b38708257
Tom Sugai <tom.sugai@gmail.com>
2024-07-03 17:34:48 +0900

    change setTo fumiko@svr.home.com

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -139,7 +139,7 @@ class ArticlesController extends AppController
                 $message = "New Post by " . $this->useremail;
                 $this->mailer
                     ->setEmailFormat('html')
-                    ->setTo('fumiko.sugai@tomsite.sakura.ne.jp')
+                    ->setTo('fumiko@svr.home.com')
                     ->setSubject('New Post')
                     ->setViewVars(['message' => $message, 'article' => $article])
                     ->viewBuilder()
