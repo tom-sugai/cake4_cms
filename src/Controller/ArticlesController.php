3c278bb6a2b4a7ba036d806301166d8b6c8cf749
Tom Sugai <tom.sugai@gmail.com>
2024-08-27 10:57:52 +0900

    set mail address to array $setTo and set array mailer->setTO()

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -143,11 +143,12 @@ class ArticlesController extends AppController
                 //debug($article);
                 $subject = "PostNo." . $article->id . "posted";
                 $message = "New Post by " . $this->useremail;
+                $setTo = ['tom.sugai@theia.ocn.ne.jp','tom_sugai@yahoo.co.jp','tom.sugai@mineo.jp','tom.sugai@outlook.jp','tom.sugai@tomsite.sakura.ne.jp','tom.sugai@gmail.com'];                  
                 $this->mailer
                     ->setEmailFormat('html')
                     //->setTo('fumiko.sugai@tomsite.sakura.ne.jp')
                     //->setTo('tom.sugai@theia.ocn.ne.jp')
-                    ->setTo(['tom.sugai@theia.ocn.ne.jp','tom_sugai@yahoo.co.jp','tom.sugai@mineo.jp','tom.sugai@outlook.jp','tom.sugai@tomsite.sakura.ne.jp','tom.sugai@gmail.com'])                    
+                    ->setTo($setTo)                    
                     ->setSubject($subject)
                     ->setViewVars(['message' => $message, 'article' => $article])
                     ->viewBuilder()
